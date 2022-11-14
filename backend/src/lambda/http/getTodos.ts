4e6ca4580551e71cf9fe3b5454993ad3e9c7a918
Abubakar Muhammad Sadiq <bbkrmuhdsaddiq@gmail.com>
2022-11-14 21:26:11 +0100

    feat: add get todo

---
+++ backend/src/lambda/http/getTodos.ts
@@ -0,0 +1,41 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+
+import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
+import { getUserId } from '../utils';
+
+// TODO: Get all TODO items for a current user
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    // Write your code here
+    const userId = getUserId(event)
+    const todos = await getTodosForUser(userId)
+
+    if (todos.count !== 0) {
+      return{
+        statusCode: 201,
+        body: JSON.stringify({
+          items: todos
+        })
+      }
+      
+    }
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: 'todos not found'
+    }
+    
+  }
+)
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
